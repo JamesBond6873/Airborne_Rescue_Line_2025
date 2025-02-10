0deb4175c0037536b303bb2044653897bb7dc431
James Bond 73 <110297851+JamesBond6873@users.noreply.github.com>
2025-02-10 00:56:25 +0000

    Francisco: Created new stable release

---
+++ SwFunctional/Stable_Release_02102025_0046/Pico_Code/Motor.h
@@ -0,0 +1,32 @@
+#ifndef Motor_h
+#define Motor_h
+
+#include <Servo.h>
+#include <Arduino.h>
+
+class Motor {
+public:
+    Motor(int pwmPin, int encoderPinA, int encoderPinB);
+    void getReady();
+    void controlMotor(int pulse);
+    int getEncoderCount() const;
+
+    void encoderISR();  // Non-static, handles interrupts for this motor
+    float getRPS();     // Return revolutions per second
+    float getRPM();     // Return revolutions per minute
+
+private:
+    Servo _ESC;
+    int _pwmPin;
+    int _encoderPinA;
+    int _encoderPinB;
+    volatile int _encoderCount;
+    unsigned long _lastUpdate;  // Track the last time the encoder was updated
+    float _timeElapsed;         // Time between encoder changes
+
+    static const int pulsesPerRevolution = 360;  // Change this according to your encoder
+    static Motor* instances[4];  // Optional for global ISR management
+    static int instanceIndex;
+};
+
+#endif
