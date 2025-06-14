39a166458e5b61c10570208f4eeed42df44c2662
James Bond 73 <fpgaspar73@gmail.com>
2025-06-14 23:00:47 +0100

    Francisco: Mid work gap controller not functional

--- InProgress/mp_manager.py
+++ InProgress/mp_manager.py
@@ -49,6 +49,7 @@ Tof_4 = manager.Value("i", -1) # Front Left Side
 Tof_5 = manager.Value("i", -1) # Back Left Side
 
 
+lineStatus = manager.Value("i", "line_detected")  # "line_detected"; "gap_detected"; "gap_avoid"; "obstacle_detected"; "obstacle_avoid"; "obstacle_orientate"; "check_silver"; "position_entry"; "position_entry_1"; "position_entry_2"; "stop"
 lineCenterX = manager.Value("i", 600)
 lineAngle = manager.Value("i", 0.)
 line_angle_y = manager.Value("i", -1)
@@ -56,6 +57,9 @@ lineDetected = manager.Value("i", False)
 turnReason = manager.Value("i", 0)
 redDetected = manager.Value("i", False)
 silverValue = manager.Value("i", 0) # 0 = Line, 1 = Silver
+gapAngle = manager.Value("i", 0)
+gapCenterX = manager.Value("i", 0)
+gapCenterY = manager.Value("i", 0)
 
 rampDetected = manager.Value("i", False)
 rampUp = manager.Value("i", False)
@@ -87,7 +91,6 @@ lineCropPercentage = manager.Value("i", 0.6)
 onIntersection = manager.Value("i", False)
 turnDirection = manager.Value("i", "straight") # "straight", "left", "right", "uTurn"
 objective = manager.Value("i", "follow_line")  # "follow_line"; "zone"; "debug"
-line_status = manager.Value("i", "line_detected")  # "line_detected"; "gap_detected"; "gap_avoid"; "obstacle_detected"; "obstacle_avoid"; "obstacle_orientate"; "check_silver"; "position_entry"; "position_entry_1"; "position_entry_2"; "stop"
 zoneStatus = manager.Value("i", "notStarted")  # "notStarted"; "begin"; "entry"; "findVictims"; "pickup_ball"; "deposit_red"; "deposit_green"; "exit"; finished
 # WARNING -- SHOULD BE NOT STARTED
 LOPstate = manager.Value("i", 0)
